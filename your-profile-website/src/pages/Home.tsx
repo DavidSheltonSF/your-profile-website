d6e958054b0815a1e84d1874240eeb5d101b3d28
DavidSheltonSF <david.faria012@gmail.com>
2025-07-25 00:08:11 -0300

    feat: finish home page

--- your-profile-website/src/pages/Home.tsx
+++ your-profile-website/src/pages/Home.tsx
@@ -1,3 +1,17 @@
+import Section from "../components/Section";
+import styles from './home.module.css';
+
 export default function Home(){
-  return<><h1>HomePage</h1></>
+  return (
+    <>
+      <Section>
+        <div className={styles.pageContainer}>
+          <div className={styles.pageWrapper}>
+            <h1 className={styles.title}>Helcome to your profile website</h1>
+            <button className={styles.getStartedButton}>Get Started</button>
+          </div>
+        </div>
+      </Section>
+    </>
+  )
 }
