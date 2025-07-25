e853c46b161dcb046d3598e277f9842686d82472
DavidSheltonSF <david.faria012@gmail.com>
2025-07-25 00:18:36 -0300

    feat(Home page): make it navigate to Sign In page when clicking on the button

--- your-profile-website/src/pages/Home.tsx
+++ your-profile-website/src/pages/Home.tsx
@@ -1,14 +1,23 @@
-import Section from "../components/Section";
-import styles from './home.module.css';
+import { useNavigate } from 'react-router-dom'
+import Section from '../components/Section'
+import styles from './home.module.css'
 
-export default function Home(){
+export default function Home() {
+  const navigate = useNavigate()
+
+  function handleSubmit() {
+    navigate('/signin')
+  }
+
   return (
     <>
       <Section>
         <div className={styles.pageContainer}>
           <div className={styles.pageWrapper}>
             <h1 className={styles.title}>Helcome to your profile website</h1>
-            <button className={styles.getStartedButton}>Get Started</button>
+            <button onClick={handleSubmit} className={styles.getStartedButton}>
+              Get Started
+            </button>
           </div>
         </div>
       </Section>
